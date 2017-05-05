4e3498ac63d7780c2718e7f57505446ec55aaab5
robertquest <robertquest@users.noreply.github.com>
2017-05-05 21:40:27 +0200

    Update wp-config.php
    For OpsWorks

--- wpconfig/files/default/wp-config.php
+++ wpconfig/files/default/wp-config.php
@@ -14,10 +14,10 @@
  * @package WordPress
  */
 
-define('WP_REDIS_HOST', '');
+define('WP_REDIS_HOST', 'wordpress.pywwys.0001.use1.cache.amazonaws.com');
  
-define('WP_HOME','');
-define('WP_SITEURL','');
+define('WP_HOME','http://wordpress-337857002.us-east-1.elb.amazonaws.com');
+define('WP_SITEURL','http://wordpress-337857002.us-east-1.elb.amazonaws.com');
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
@@ -30,7 +30,7 @@ define('DB_USER', 'wordpress');
 define('DB_PASSWORD', 'wordpress');
 
 /** MySQL hostname */
-define('DB_HOST', '');
+define('DB_HOST', 'wordpress.chk4akynbse7.us-east-1.rds.amazonaws.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
