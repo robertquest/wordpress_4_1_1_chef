692181b07a50cbc409f2ca63f7b278046c8741a1
robertquest <robertquest@users.noreply.github.com>
2017-05-05 22:16:54 +0200

    Update wp-config.php

--- wpconfig/files/default/wp-config.php
+++ wpconfig/files/default/wp-config.php
@@ -16,8 +16,8 @@
 
 define('WP_REDIS_HOST', 'wordpress.pywwys.0001.use1.cache.amazonaws.com');
  
-define('WP_HOME','http://wordpress-337857002.us-east-1.elb.amazonaws.com');
-define('WP_SITEURL','http://wordpress-337857002.us-east-1.elb.amazonaws.com');
+define('WP_HOME','http://d64sw7e4h9pal.cloudfront.net');
+define('WP_SITEURL','http://d64sw7e4h9pal.cloudfront.net');
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
